d34112e7b8caa675f39c3d1329b1b8a2e76701e3
Maciej Kawa <kawciaaak@gmail.com>
2024-03-06 16:07:48 +0100

    Quick fix of Collection class (missing virtual method declaration)

--- figures/segment/Segment.h
+++ figures/segment/Segment.h
@@ -7,19 +7,22 @@
 class Segment : public Figure
 {
 public:
-    Segment() = default;
-    Segment(const Point& first_point, const Point& second_point);
-    ~Segment() = default;
+  Segment() = default;
+  Segment(const Point &first_point, const Point &second_point);
+  ~Segment() = default;
 
-    double GetLength() const;
-    std::string GetObjectName() const override;
-    std::string GetObjectCoordinate() const override;
+  double GetLength() const;
+  std::string GetObjectName() const override;
+  std::string GetObjectCoordinate() const override;
 
-    bool IsPointOnSegment(const Point& point_to_verify);
+  bool IsPointOnSegment(const Point &point_to_verify);
 
-    bool AreSegmentsParallel(const Segment& other_segment);
-    bool AreSegmentsPerpendicular(const Segment& other_segment);
-    void GetIntersectionPoint(const Segment& other_segment);
+  bool AreSegmentsParallel(const Segment &other_segment);
+  bool AreSegmentsPerpendicular(const Segment &other_segment);
+  void GetIntersectionPoint(const Segment &other_segment);
+
+  void CreateLinearFunction(const Point &, const Point &, const double &, const double &) override;
+  bool IsPointOnLinearFunction(const Point &, const double &, const double &) override;
 };
 
 #endif // FIGURES_FIGURES_SEGMENT_SEGMENT_H
